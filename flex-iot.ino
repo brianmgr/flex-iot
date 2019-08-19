b67524dac21b340e63da9a1ef216df62dc81f038
Brian Mgrdichian <brian@BreezusMBP.local>
2019-08-19 07:52:24 -0600

    Initial Commit

---
+++ flex-iot.ino
@@ -0,0 +1,51 @@
+#include <SoftwareSerial.h>
+
+// Create software serial object to communicate with SIM800L
+SoftwareSerial mySerial(10, 11); //SIM800L Tx & Rx, respectively
+
+// Temperature and humidity sensor
+#include "DHT.h"
+#define DHTPIN 6 // Sensor data pin
+#define DHTTYPE DHT11 // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+
+
+void setup()
+{
+  //Begin serial communication with Arduino and Arduino IDE (Serial Monitor)
+  Serial.begin(9600);
+  
+  //Begin serial communication with Arduino and SIM800L
+  mySerial.begin(9600);
+
+  Serial.println("Initializing..."); 
+  delay(1000);
+
+  mySerial.println("AT"); //Once the handshake test is successful, it will back to OK
+  updateSerial();
+
+  mySerial.println("AT+CMGF=1"); // Configuring TEXT mode
+  updateSerial();
+  mySerial.println("AT+CMGS=\"2963\"");//change ZZ with country code and xxxxxxxxxxx with phone number to sms
+  updateSerial();
+  mySerial.print("android"); //text content
+  updateSerial();
+  mySerial.write(26);
+}
+
+void loop()
+{
+}
+
+void updateSerial()
+{
+  delay(500);
+  while (Serial.available()) 
+  {
+    mySerial.write(Serial.read());//Forward what Serial received to Software Serial Port
+  }
+  while(mySerial.available()) 
+  {
+    Serial.write(mySerial.read());//Forward what Software Serial received to Serial Port
+  }
+}
